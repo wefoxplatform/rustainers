8e439c01d23485693ff9fc7e2088c4ca23863b0f
Igor Laborie <igor.laborie@wefox.com>
2024-04-19 15:49:49 +0200

    chore: configure lints at workspace level

--- rustainers/src/lib.rs
+++ rustainers/src/lib.rs
@@ -1,8 +1,3 @@
-#![warn(missing_docs)]
-#![forbid(unsafe_code)]
-#![warn(clippy::perf)]
-#![warn(clippy::pedantic)]
-#![allow(clippy::module_name_repetitions)]
 #![doc = include_str!("../README.md")]
 
 mod error;
